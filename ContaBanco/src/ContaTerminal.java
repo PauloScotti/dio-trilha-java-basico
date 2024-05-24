34b09a0068b7b0b548dca295569e2922eb58aedc
PauloScotti <44655524+PauloScotti@users.noreply.github.com>
2024-05-24 14:10:08 -0300

    desafio de conta banco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        int numero = 0;
+        String agencia = "";
+        String nomeCliente = "";
+        double saldo = 0.00;
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o seu nome:");
+        nomeCliente = scanner.next();
+        System.out.println("Por favor, digite o número da Agência!");
+        agencia = scanner.next();
+        System.out.println("Por favor, digite o número da conta!");
+        numero = scanner.nextInt();
+        System.out.println("Por favor, digite o saldo da conta!");
+        saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+    }
+}
